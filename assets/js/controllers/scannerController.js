1aea968feb810189b9a30dedc13be506087cc4cf
wongsrila <jiraphong.wongsrila@gmail.com>
2023-03-06 16:33:55 +0100

    6mar 16:33

--- assets/js/controllers/scannerController.js
+++ assets/js/controllers/scannerController.js
@@ -5,6 +5,21 @@ const scannerGet = () => {
     <main>
       <div id="reader"></div>
     </main>
+    <footer>
+      <div class="container">
+        <nav>
+          <ul>
+            <li><a href="./">Home</a></li>
+            <li>
+              <a href="#scanner">
+                <img src="assets/images/barcode_icon.png" alt="" />
+              </a>
+            </li>
+            <li>Settings</li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
   `;
   app.innerHTML = markup;
 
@@ -22,16 +37,15 @@ const scannerGet = () => {
       console.log(err);
     });
 
-  html5QrCode
-    .stop()
-    .then((ignore) => {
-      // QR Code scanning is stopped.
-    })
-    .catch((err) => {
-      // Stop failed, handle it.
-    });
-
   const markupData = (decodedData) => {
+    html5QrCode
+      .stop()
+      .then((ignore) => {
+        // QR Code scanning is stopped.
+      })
+      .catch((err) => {
+        // Stop failed, handle it.
+      });
     routie(`results/${decodedData}`);
   };
 };
