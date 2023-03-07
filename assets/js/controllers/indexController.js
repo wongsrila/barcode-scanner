e6b30eeba5a35c5c1467246b375b900757416527
wongsrila <jiraphong.wongsrila@gmail.com>
2023-03-07 19:32:55 +0100

    7mar 19:32

--- assets/js/controllers/indexController.js
+++ assets/js/controllers/indexController.js
@@ -151,16 +151,49 @@ const indexGet = () => {
   // add items to a table that are saved in the localstorage
 
   const items = JSON.parse(localStorage.getItem('items')) || [];
+
+  // for (let index = 0; index < items.length; index++) {
+  //   const item = items[index];
+  //   ul.innerHTML += `
+  //   <li>
+  //     <div class="item-img__wrapper"><img src="${item.imgUrl}" /></div>
+  //     <div class="item-info__wrapper">
+  //       <p>${item.name} - ${item.quantity}</p>
+  //       <p class="is--text-grey">
+  //         <span class="is--text-green">${item.energy} kcal</span> - ${item.ingredients}</p>
+  //     </div>
+  //     <div class="item-edit_wrapper">
+  //       <a class="more-info" onclick="myFunction(${item.barcode})">
+  //         <svg
+  //           width="20"
+  //           height="20"
+  //           viewBox="0 0 20 20"
+  //           fill="none"
+  //           xmlns="http://www.w3.org/2000/svg"
+  //         >
+  //           <path
+  //             d="M7.14648 7.07361C7.31728 6.54732 7.63015 6.07896 8.05078 5.71948C8.47141 5.36001 8.9838 5.12378 9.53027 5.03708C10.0768 4.95038 10.6362 5.0164 11.1475 5.22803C11.6587 5.43966 12.1014 5.78875 12.4268 6.23633C12.7521 6.68391 12.9469 7.21256 12.9904 7.76416C13.0339 8.31576 12.9238 8.86879 12.6727 9.36182C12.4215 9.85484 12.0394 10.2685 11.5676 10.5576C11.0958 10.8467 10.5533 10.9998 10 10.9998V12.0002M10 19C5.02944 19 1 14.9706 1 10C1 5.02944 5.02944 1 10 1C14.9706 1 19 5.02944 19 10C19 14.9706 14.9706 19 10 19ZM10.0498 15V15.1L9.9502 15.1002V15H10.0498Z"
+  //             stroke="#7b7b7b"
+  //             stroke-width="2"
+  //             stroke-linecap="round"
+  //             stroke-linejoin="round"
+  //           />
+  //         </svg>
+  //       </a>
+  //     </div>
+  //   </li>
+  //   `;
+  // }
+
   const ul = document.querySelector('ul');
 
   items.forEach((item) => {
     let li = document.createElement('li');
-    li.innerHTML = `
+    li.innerHTML += `
       <div class="item-img__wrapper"><img src="${item.imgUrl}" /></div>
       <div class="item-info__wrapper">
         <p>${item.name} - ${item.quantity}</p>
-        <p class="is--text-grey">
-          <span class="is--text-green">${item.energy} kcal</span> - ${item.ingredients}</p>
+        <p class="is--text-grey"><span class="is--text-green">${item.energy} kcal</span> - ${item.ingredients}</p>
       </div>
       <div class="item-edit_wrapper">
         <a class="more-info">
@@ -183,15 +216,31 @@ const indexGet = () => {
       </div>
     `;
 
-    ul.append(li);
-
-    const infoBtn = document.querySelector('.more-info');
-
-    infoBtn.addEventListener('click', () => {
-      console.log(item.barcode);
-      // routie(`edit/90162602`);
+    li.addEventListener('click', function () {
+      // console.log(item.barcode);
+      routie(`edit/${item.barcode}`);
     });
+
+    ul.appendChild(li);
   });
+
+  // const infoBtn = document.querySelector('.more-info');
+
+  // (function (index) {
+  //   infoBtn.addEventListener('click', function () {
+  //     console.log(index);
+  //   });
+  // })(item.barcode);
+
+  //   infoBtn.addEventListener('click', () => {
+  //     console.log(item.barcode);
+  //     routie(`edit/90162602`);
+  //   });
+  // });
+};
+
+const myFunction = (barcode) => {
+  console.log(barcode);
 };
 
 export default indexGet;
